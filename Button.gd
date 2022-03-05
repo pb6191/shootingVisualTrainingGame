f62016dbb70394a7aeefa00fe6d144f80e09231e
Puneet Bhargava <pbhargava936@gmail.com>
2022-03-05 10:13:21 -0800

    refactoring

--- Button.gd
+++ Button.gd
@@ -18,6 +18,8 @@ var OexpectedShapes = ["sphere", "capsule", "prism", "cube", "cylinder"]
 var OexpectedColors = ["000", "001", "010", "011", "100", "101", "110", "111"]
 var OexpectedOrients = ["x", "y"]
 
+var temp
+
 func checkPresence(arr1, arr2):
 	var num2 = 0
 	for i in arr1:
@@ -65,18 +67,92 @@ func _on_Button_pressed():
 	global.OshapeArr = global.Oshape.split(",")
 	global.OcolorArr = global.Ocolor.split(",")
 	global.OorientArr = global.Oorient.split(",")
+	while (global.sizeArr.size() < global.levels):
+		temp = global.sizeArr
+		temp.push_back('')
+		global.sizeArr = temp
+	while (global.speedArr.size() < global.levels):
+		temp = global.speedArr
+		temp.push_back('')
+		global.speedArr = temp
+	while (global.styleArr.size() < global.levels):
+		temp = global.styleArr
+		temp.push_back('')
+		global.styleArr = temp
+	while (global.shapeArr.size() < global.levels):
+		temp = global.shapeArr
+		temp.push_back('')
+		global.shapeArr = temp
+	while (global.colorArr.size() < global.levels):
+		temp = global.colorArr
+		temp.push_back('')
+		global.colorArr = temp
+	while (global.orientArr.size() < global.levels):
+		temp = global.orientArr
+		temp.push_back('')
+		global.orientArr = temp
+	while (global.OsizeArr.size() < global.levels):
+		temp = global.OsizeArr
+		temp.push_back('')
+		global.OsizeArr = temp
+	while (global.OspeedArr.size() < global.levels):
+		temp = global.OspeedArr
+		temp.push_back('')
+		global.OspeedArr = temp
+	while (global.OstyleArr.size() < global.levels):
+		temp = global.OstyleArr
+		temp.push_back('')
+		global.OstyleArr = temp
+	while (global.OshapeArr.size() < global.levels):
+		temp = global.OshapeArr
+		temp.push_back('')
+		global.OshapeArr = temp
+	while (global.OcolorArr.size() < global.levels):
+		temp = global.OcolorArr
+		temp.push_back('')
+		global.OcolorArr = temp
+	while (global.OorientArr.size() < global.levels):
+		temp = global.OorientArr
+		temp.push_back('')
+		global.OorientArr = temp
+	for k1 in global.levels:
+		if global.sizeArr[k1] == '':
+			global.sizeArr[k1] = 'large'
+		if global.speedArr[k1] == '':
+			global.speedArr[k1] = 'zero'
+		if global.styleArr[k1] == '':
+			global.styleArr[k1] = 'straight'
+		if global.shapeArr[k1] == '':
+			global.shapeArr[k1] = 'sphere'
+		if global.colorArr[k1] == '':
+			global.colorArr[k1] = '001'
+		if global.orientArr[k1] == '':
+			global.orientArr[k1] = 'x'
+		if global.OsizeArr[k1] == '':
+			global.OsizeArr[k1] = 'zero'
+		if global.OspeedArr[k1] == '':
+			global.OspeedArr[k1] = 'zero'
+		if global.OstyleArr[k1] == '':
+			global.OstyleArr[k1] = 'straight'
+		if global.OshapeArr[k1] == '':
+			global.OshapeArr[k1] = 'sphere'
+		if global.OcolorArr[k1] == '':
+			global.OcolorArr[k1] = '000'
+		if global.OorientArr[k1] == '':
+			global.OorientArr[k1] = 'x'
 	if !checkPresence(global.orientArr, expectedOrients) or !checkPresence(global.colorArr, expectedColors) or !checkPresence(global.shapeArr, expectedShapes) or !checkPresence(global.sizeArr, expectedSizes) or !checkPresence(global.speedArr, expectedSpeeds) or !checkPresence(global.styleArr, expectedStyles):
 		num = 1
 		$"../RichTextLabel5".text = "Unexpected value(s) in parameters"
-	if global.orientArr.size() != global.levels or global.colorArr.size() != global.levels or global.shapeArr.size() != global.levels or global.sizeArr.size() != global.levels or global.speedArr.size() != global.levels or global.styleArr.size() != global.levels:
-		num = 1
-		$"../RichTextLabel5".text = "Mismatch between the number of levels and the number of parameter values"
+	#if global.orientArr.size() != global.levels or global.colorArr.size() != global.levels or global.shapeArr.size() != global.levels or global.sizeArr.size() != global.levels or global.speedArr.size() != global.levels or global.styleArr.size() != global.levels:
+	#	num = 1
+	#	$"../RichTextLabel5".text = "Mismatch between the number of levels and the number of parameter values"
 	if !checkPresence(global.OorientArr, OexpectedOrients) or !checkPresence(global.OcolorArr, OexpectedColors) or !checkPresence(global.OshapeArr, OexpectedShapes) or !checkPresence(global.OsizeArr, OexpectedSizes) or !checkPresence(global.OspeedArr, OexpectedSpeeds) or !checkPresence(global.OstyleArr, OexpectedStyles):
 		num = 1
 		$"../RichTextLabel5".text = "Unexpected value(s) in parameters"
-	if global.OorientArr.size() != global.levels or global.OcolorArr.size() != global.levels or global.OshapeArr.size() != global.levels or global.OsizeArr.size() != global.levels or global.OspeedArr.size() != global.levels or global.OstyleArr.size() != global.levels:
-		num = 1
-		$"../RichTextLabel5".text = "Mismatch between the number of levels and the number of parameter values"
+	#if global.OorientArr.size() != global.levels or global.OcolorArr.size() != global.levels or global.OshapeArr.size() != global.levels or global.OsizeArr.size() != global.levels or global.OspeedArr.size() != global.levels or global.OstyleArr.size() != global.levels:
+	#	num = 1
+	#	$"../RichTextLabel5".text = "Mismatch between the number of levels and the number of parameter values"
+
 	if num == 0:
 		global.midScrnText = "Click 'Next Level' to start the first level."
 		global.nextLevelVis = true
--- StaticBody10.gd
+++ StaticBody10.gd
@@ -14,7 +14,7 @@ func _ready():
 	rng.randomize()
 	$".".translation.x = rng.randi_range(-10,10)
 	rng.randomize()
-	$".".translation.y = rng.randi_range(10,30)
+	$".".translation.y = rng.randi_range(0,20)
 	if global.OspeedArr[global.currCounter] == "slow":
 		targetSpeed = 10
 	if global.OspeedArr[global.currCounter] == "fast":
@@ -40,13 +40,13 @@ func _physics_process(delta):
 		if global.OstyleArr[global.currCounter] == "sine":
 			$".".translation.y = initY + 10*sin(0.314*$".".translation.x)
 	if global.OorientArr[global.currCounter] == "y":
-		if $".".translation.y <= 30 and targetDir == "right":
+		if $".".translation.y <= 20 and targetDir == "right":
 			$".".translation.y += 0.017*targetSpeed
-		if $".".translation.y >= 10 and targetDir == "left":
+		if $".".translation.y >= 0 and targetDir == "left":
 			$".".translation.y -= 0.017*targetSpeed
-		if $".".translation.y >= 30:
+		if $".".translation.y >= 20:
 			targetDir = "left"
-		if $".".translation.y <= 10:
+		if $".".translation.y <= 0:
 			targetDir = "right"
 		if global.OstyleArr[global.currCounter] == "sine":
 			$".".translation.x = initX + 10*sin(0.314*$".".translation.y)
